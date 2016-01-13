763f9de42baaebe544a3bcce5485a6061e86119d
James Witkowski <jameswitkowski@James-Witkowskis-MacBook-Pro.local>
2016-01-13 12:33:02 -0700

    functions for game

---
+++ game.js
@@ -0,0 +1,20 @@
+var health = 100;
+function slap() {
+    for ( health = 99; health > 0; health--){
+    }
+}
+
+slap();
+
+function update() {
+   var x = document.getElementById("HEALTH");
+   x.innerText =   
+}
+
+update();
+
+
+
+
+
+
